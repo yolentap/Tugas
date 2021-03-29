fda344fac580aa58510fe711377ddcbb214b9d60
Michelle Pardede <michellepardede08@gmail.com>
2021-03-29 12:50:22 +0700

    Menambahkan file baru bernama mahasiswa.php

---
+++ mahasiswa.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Form Mahasiswa</title>
+</head>
+<body>
+<h2>Form Mahasiswa</h2>
+ <form method = 'post' action= "aksiForm.php">
+  <table>
+   <tr>
+    <td>Nama</td>
+    <td>:</td>
+    <td><input type='text' name='nama'></td>
+   </tr>
+   <tr>
+    <td>Umur</td>
+    <td>:</td>
+    <td><input type='text' name='umur'></td>
+   </tr>
+   <tr>
+    <td>Alamat</td>
+    <td>:</td>
+    <td><input type='text' name='alamat'></td>
+   </tr>
+   <tr>
+    <td></td>
+    <td></td>
+    <td><input type='submit' value='KIRIM'></td>
+   </tr>
+  </table>  
+ </form>
+</body>
+</html>
